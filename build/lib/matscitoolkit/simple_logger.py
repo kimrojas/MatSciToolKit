c147cb7d9b4348dd4f6b263d0b6a4d68c989b949
kimrojas <kurt.irvin.rojas@gmail.com>
2023-11-07 14:42:50 +0900

    delete

--- build/lib/matscitoolkit/simple_logger.py
+++
@@ -1,9 +0,0 @@
-from pprint import pprint
-
-# Accepts string and writes to logfile
-def print_log(string: str, logfile: str = "MatciSciToolkit.log"):
-    print(string)
-    with open(logfile, "a") as f:
-        f.write(string + "\n")
-
-
--- build/lib/matscitoolkit/utils/parallel.py
+++
@@ -1,8 +0,0 @@
-from multiprocess import Pool
-from tqdm import tqdm
-
-def process_map(f, args, max_workers, desc):
-    with Pool(max_workers) as p:
-        results = list(tqdm(p.imap(f, args), desc=desc, total=len(args)))
-
-    return results
