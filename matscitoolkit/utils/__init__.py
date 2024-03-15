178785d9b1fddf82123f9a5b87709eeadfbff251
kimrojas <kurt.irvin.rojas@gmail.com>
2024-03-15 15:27:25 +0900

    MAINT: added proper versioning

---
+++ matscitoolkit/utils/__init__.py
@@ -0,0 +1,3 @@
+from matscitoolkit.__version__ import __version__
+
+__version__ = __version__
--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup, find_packages
+import re
+
+# Version Number:
+version_file = "matscitoolkit/__version__.py"
+with open(version_file) as f:
+    lines = f.readlines()
+
+for line in lines:
+    if "__version_info__" in line:
+        result = re.findall("\d+", line)
+        result = [int(x) for x in result]
+        version = "{}.{}.{}".format(*result)
+        break
+
 
 setup(
     name="matscitoolkit",
-    python_requires='>3.9',
-    version="0.1",
+    python_requires=">3.9",
+    version=version,
     packages=find_packages(),
     install_requires=[
         "numpy==1.22",
@@ -13,5 +27,5 @@ setup(
         "scipy",
         "pytest",
         "multiprocess",
-    ]
+    ],
 )
