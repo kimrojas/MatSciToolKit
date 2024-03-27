9c837d0f1c853c5d6a760e66e9643e01327c2a42
kimrojas <kurt.irvin.rojas@gmail.com>
2024-03-27 14:55:47 +0900

    finalized ABC_workflow

--- matscitoolkit/analysis_workflow/ABC_workflow.py
+++ matscitoolkit/analysis_workflow/ABC_workflow.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from ase.io import read, write
+from ase.vibrations import Vibrations, Infrared
 from pathlib import Path
 from matscitoolkit.analysis_workflow.logger import logger
 
@@ -26,10 +27,8 @@ class WorkflowBaseClass(ABC):
         self.filetype = self.filepath.suffix
 
         # Initialize logger
-        nfiles = 1 + (len(read(self.filepath)) * 6)
-        dim = len(str(nfiles))
         self.jobnumber = int(jobnumber)
-        self.log = logger(logfile=f"{self.filestem}_{self.jobnumber:0{dim}d}.log", debug=debug)
+        self.log = logger(logfile=f"{self.filestem}_{self.jobnumber}.log", debug=debug)
         self.log.info(f"Subclass name: {self.__class__.__name__}s")
 
         # Initialize cache directory
@@ -37,12 +36,53 @@ class WorkflowBaseClass(ABC):
         self.cache.mkdir(exist_ok=True)
         self.log.info(f"Cache directory created: {str(self.cache)}")
 
-        # Input checks
+    def get_displaced_structure(self, generatefile=True, directory=None, methodkwargs={}):
+        """Produces the displaced structure for a given job number"""
+
+        # Add default values for methodkwargs
+        default_methodkwargs = {"indices": None, "delta": 0.01, "nfree": 2, "directions": None}
+        for k, v in default_methodkwargs.items():
+            methodkwargs.setdefault(k, v)
+
+        # Compute all displaced structures
         self.log.info(f"Reference structure: '{self.filename}'")
-        self.log.info(f"Expected number of displaced structures: {nfiles}")
+        displaced_structures = dict(enumerate(Infrared(read(self.filepath), **methodkwargs).iterdisplace(), start=1))
+        self.nfiles = len(displaced_structures)
+        self.dim = len(str(self.nfiles))
+        self.log.info(f"Expected number of displaced structures: {self.nfiles}")
+
+        # Select/Map the job number to the displaced structure
         self.log.info(f"Job number: {self.jobnumber}")
-        self.log.assert_(1 <= self.jobnumber <= nfiles, f"Job number {self.jobnumber} is out of range [1-{nfiles}]")
-    
+        self.log.assert_(
+            1 <= self.jobnumber <= self.nfiles, f"Job number {self.jobnumber} is out of range [1-{self.nfiles}]"
+        )
+        disp, atm = displaced_structures[self.jobnumber]
+
+        # Organize job information
+        self.log.info(f"Job name: {disp.name}")
+        self.job = {"number": self.jobnumber, "name": disp.name, "structure": atm}
+
+        # Print/Output structure file for displaced structure
+        if directory is None:
+            directory = self.cache / "displaced_structures"
+        else:
+            directory = self.cache / directory
+
+        if generatefile:
+            dispfile = directory / f"{self.jobnumber:0{self.dim}d}.{self.job['name']}{self.filetype}"
+            directory.mkdir(exist_ok=True)  # Initialize directory
+            write(dispfile, self.job["structure"])  # Write structure file
+            self.log.info(f"Displaced structure saved in: {str(dispfile)}")
+
+    @abstractmethod
+    def run(self):
+        """RUN DFT CALCULATION on self.job['structure']"""
+        pass
+
+    @abstractmethod
+    def clean(self, directory=None):
+        """CLEAN TEMPORARY DIRECTORY"""
+        pass
+
     def close_logger(self):
         self.log.close()
-
